41e821a18cb9e96074e732602d4a3460e1feb4a5
Student P_10 <studentp_10@felixs-macbook-pro.local>
2016-10-21 12:30:52 +0530

    Initial Commit

---
+++ MFgreaterandsmaller/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MFgreaterandsmaller
+//
+//  Created by Student P_10 on 21/10/16.
+//  Copyright © 2016 Mohsin Farooqui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
